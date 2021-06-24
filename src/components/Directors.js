336b721cc7b276e4273a6a1adcffe4c8f790df97
Brian Smith <brian.bubbam@gmail.com>
2021-06-24 11:54:02 -0500

    commit tree d65055c8b56743bd2b66f0e13ba7e1da8c8536be

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+
+  const directorsInfo = directors.map((director) => (
+    <div key={director.name}>
+      <h2>Name: {director.name}</h2>
+      <p>Movies:</p>
+    <ul>
+      {director.movies.map((movie) => ( // when you do the second map you have to use the arguments
+        <li key={movie}>{movie}</li> //passed through (director) because you are no longer going through directors
+      ))}
+    </ul>
+    </div>
+  ))
+
+  return ( 
+    <div>
+      <h1>Directors Page</h1>
+      {directorsInfo}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,34 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+
+
+  //run the map up here and set up the JSX
+  //so you can just add the variable movieItems in the JSX below
+  const movieItems = movies.map((movie) => ( // this is a function saved in a variable
+    <div key={movie.title}>
+      <h2>Name: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {movie.genres.map((genre) => ( // if you do another map you have to open up the curly brackets agin
+          <li key={genre}>{genre}</li> // you need to use movie instead of movies beucase you are mapping through
+                                      // that particula movie
+          ))}                          
+      </ul>
+    </div>
+  ))
+
+  
+
+  return ( 
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+
+    </div>
+  )
 }
 
 export default Movies;
